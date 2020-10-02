70c893cd12b9b906e260d315719b3a6a3e51d81a
“dengs” <1019217919@qq.com>
2020-10-02 14:14:29 +0800

    feat:下表为-1表示最后一个元素

--- code/简单消息.py
+++ code/简单消息.py
@@ -59,3 +59,7 @@ bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
 # 下标访问列表
 print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
