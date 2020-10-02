4f6b40156a887fd8f91a69daa67bef2d97ba3dba
“dengs” <1019217919@qq.com>
2020-10-02 14:23:13 +0800

    feat:下表取列表元素的一些实用场景

--- code/简单消息.py
+++ code/简单消息.py
@@ -63,3 +63,24 @@ print(bicycles[0].title())
 print(bicycles[1])
 print(bicycles[3])
 print(bicycles[-1])
+# 负数索引表示反向取数据，-1表示最后一个元素，-2表示倒数第二个元素，以此类推。。。
+message = "我的第一辆自行车是 " + bicycles[0].title() + "."
+print(message)
+
+# 3-1 姓名： 将一些朋友的姓名存储在一个列表中，并将其命名为 names 。依次访问该列表中的每个元素，从而将每个朋友的姓名都打印出来。
+names = ["王欣", "蒲佳", "李敏"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[-1])
+print(names[-2])
+print(names[-3])
+# 3-2 问候语： 继续使用练习 3-1 中的列表，但不打印每个朋友的姓名，而为每人打印一条消息。
+# 每条消息都包含相同的问候语，但抬头为相应朋友的姓名。
+print(names[0] + '是一个男朋友')
+print(names[1] + '也是一个男朋友')
+print(names[2] + '是一个女朋友')
+# 3-3 自己的列表： 想想你喜欢的通勤方式，如骑摩托车或开汽车，并创建一个包含多种通勤方式的列表。
+# 根据该列表打印一系列有关这些通勤方式的宣言，如 “I would like to own a Honda motorcycle”
+types = ['走路', '骑自行车', '坐公交车', '打车', '自驾探险者']
+print('我更喜欢' + types[-1] + '去上班。')
